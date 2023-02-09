739786c52a55691708e1edcd816f356eccd49158
pablogalle <pablo.gallego.penas@cieep.com>
2023-02-09 01:12:42 +0100

    webApiSeries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SeriesListComponent} from "./components/series-list/series-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: SeriesListComponent},
+  {path: 'series', redirectTo: ''},
+  {path: '**', redirectTo: '/series', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeriesListComponent } from './components/series-list/series-list.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeriesListComponent,
+    NavigationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
